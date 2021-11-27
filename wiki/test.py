3ea2ca3d7862929fca4d151abaefb0ae57482d0b
Nicola Toscan <nicolatoscan99@outlook.it>
2021-11-27 18:42:04 +0100

    create json file

--- wiki/test.py
+++ wiki/test.py
@@ -5,16 +5,21 @@ from tools import files, userdata
 from datetime import datetime, timedelta
 import numpy as np
 from itertools import groupby
-
+import json
+# import pymongo
 
 # %% load user data
-lang = 'es'
+typeAction = 'user'
+lang = 'it'
 uData = userdata.loadUserData(Path(f'../dataset/genders/genders-{lang}.tsv'))
 print(f'Loaded {lang}')
 
+# %% setup mongo
+# dbColl = pymongo.MongoClient("mongodb://localhost:27017/")["wiki"][f"{typeAction}-{lang}"]
 
 # %%
-savingFile = open(f'reply-{lang}.svg', 'w')
+savingFile = open(f'{typeAction}-{lang}.csv', 'w')
+savingFileJson = open(f'{typeAction}-{lang}.json', 'w')
 
 def analyze(id: int, data: List[dict]) -> None:
     gender = -1
@@ -30,9 +35,18 @@ def analyze(id: int, data: List[dict]) -> None:
     revs = []
     for k,vals in groupby(data,key=lambda x:x['id'].split('.')[0]):
         vals = [v for v in vals]
-        emotions = np.sum([ [ int(e) for e in v['emotions'].split(',')[:-1] ] for v in vals ], axis=0)
+        filtredVals = [v for v in vals if v['type'][0] == 'A' or v['type'][0] == 'C' ]
+        emotions = np.sum(
+            [ 
+                [ int(e) for e in v['emotions'].split(',')[:-1] ]
+                for v in filtredVals
+            ],
+            axis=0
+        ) if len(filtredVals) > 0 else np.zeros(11)
+
         nEmotions = emotions[0]
         emotions = emotions[1:]
+
         revs.append({
             "id": k,
             "type": [ v['type'] for v in vals ],
@@ -84,7 +98,7 @@ def analyze(id: int, data: List[dict]) -> None:
 
     lastRev = revs
     perEmotionsLastDays = []
-    for n in [10000, 365, 180, 90, 60, 30, 7]:
+    for n in [ 10000, 365, 180, 90, 60, 30, 7 ]:
         d = lastEditDate - timedelta(days=n)
         lastRev = [ r for r in lastRev if r['timestamp'] > d ]
         nEmo = np.sum( [ r['nEmotions'] for r in lastRev ])
@@ -106,9 +120,27 @@ def analyze(id: int, data: List[dict]) -> None:
     ] + [ f"{x[0]}|{x[1]}|{','.join( [ str(i) for i in list(x[2]) ] )}"  for x in perEmotionsLastDays]
     savingFile.write('\t'.join( [str(x) for x in lineParams ] ) + '\n')
 
-filesList = files.getFileList(f'../dataset/min/reply/{lang}', '*.gz')
+    d = {
+        "id": id,
+        "gender": gender,
+        "roles": roles,
+        "name": name,
+        "nEdit": nEdit,
+        "firstEditDate": firstEditDate.isoformat(),
+        "lastEditDate": lastEditDate.isoformat(),
+        "avgDiffTime": avgDiffTime,
+        "editTypes": perTypeEdits,
+        "namespaces": perNamespaces,
+        "emotionsLastDays": [ { "n": int(x[0]), "nEmo": int(x[1]), "avg": x[2].tolist() } for x in perEmotionsLastDays ]
+    }
+
+    savingFileJson.write(f'{json.dumps(d)}\n')
+
+
+filesList = files.getFileList(f'../dataset/min/{typeAction}/{lang}', '*.gz')
 files.readFileSections(filesList, analyze)
 
 savingFile.close()
+savingFileJson.close()
 print("Done")
 # %%
